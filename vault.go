84594135241d6ff453d1a700987672a5e9013b25
Pratiyush <pushkarpratiyush@gmail.com>
2023-02-18 23:22:40 +0530

    Renew vault auth token

--- vault.go
+++ vault.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/hashicorp/vault/api"
@@ -20,7 +21,10 @@ type vault struct {
 
 // NewVault creates a vault client to talk with underline vault server
 func newVault(address string, prefix string, token string) vault {
-	return vault{address, prefix, token}
+	v := vault{address, prefix, token}
+
+	go v.newVaultClientToRenewToken()
+	return v
 }
 
 func (v vault) Store(msg string, ttl string) (token string, err error) {
@@ -36,7 +40,7 @@ func (v vault) Store(msg string, ttl string) (token string, err error) {
 	}
 
 	// validate duration length
-	if d > 168 * time.Hour || d == 0 * time.Hour  {
+	if d > 168*time.Hour || d == 0*time.Hour {
 		return "", fmt.Errorf("cannot set ttl to infinte or more than 7 days %v", err)
 	}
 
@@ -104,7 +108,7 @@ func (v vault) writeMsgToVault(token, msg string) error {
 
 	raw := map[string]interface{}{"msg": msg}
 
-	_, err = c.Logical().Write("/" + v.prefix + token, raw)
+	_, err = c.Logical().Write("/"+v.prefix+token, raw)
 
 	return err
 }
@@ -131,3 +135,43 @@ func (v vault) newVaultClientWithToken(token string) (*api.Client, error) {
 	return c, nil
 }
 
+func (v vault) newVaultClientToRenewToken() {
+	c, err := v.newVaultClient()
+	if err != nil {
+		log.Println(err)
+	}
+	client_auth_token := &api.Secret{Auth: &api.SecretAuth{ClientToken: c.Token(), Renewable: true}}
+
+	/* */
+	log.Println("renew cycle: begin")
+	defer log.Println("renew cycle: end")
+
+	// auth token
+	authTokenWatcher, err := c.NewLifetimeWatcher(&api.LifetimeWatcherInput{
+		Secret: client_auth_token,
+	})
+
+	if err != nil {
+		err := fmt.Errorf("unable to initialize auth token lifetime watcher: %w", err)
+		fmt.Println(err.Error())
+	}
+
+	go authTokenWatcher.Start()
+	defer authTokenWatcher.Stop()
+
+	// monitor events from both watchers
+	for {
+		select {
+
+		case err := <-authTokenWatcher.DoneCh():
+			// Leases created by a token get revoked when the token is revoked.
+			fmt.Println("Error is :", err)
+
+		// RenewCh is a channel that receives a message when a successful
+		// renewal takes place and includes metadata about the renewal.
+		case info := <-authTokenWatcher.RenewCh():
+			log.Printf("auth token: successfully renewed; remaining duration: %ds", info.Secret.Auth.LeaseDuration)
+		}
+	}
+
+}
