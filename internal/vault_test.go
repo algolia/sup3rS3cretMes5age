63d43e2735021528a3c2a587ab6a3d80168c8b1e
Jeremy JACQUE <jeremy.jacque@algolia.com>
2024-06-09 09:42:34 +0200

    fix: use hashicorp/vault image cause vault image is deprecated

--- internal/vault_test.go
+++ internal/vault_test.go
@@ -19,7 +19,13 @@ func TestMain(m *testing.M) {
 	}
 
 	// pulls an image, creates a container based on it and runs it
-	resource, err := pool.Run("vault", "latest", []string{"VAULT_ADDR", "VAULT_DEV_ROOT_TOKEN_ID=rootsecret"})
+	dro := &dockertest.RunOptions{
+		Repository: "hashicorp/vault",
+		Tag:        "latest",
+		Env:        []string{"VAULT_DEV_ROOT_TOKEN_ID=rootsecret"},
+		CapAdd:     []string{"IPC_LOCK"},
+	}
+	resource, err := pool.RunWithOptions(dro)
 	if err != nil {
 		log.Fatalf("Could not start resource: %s", err)
 	}
